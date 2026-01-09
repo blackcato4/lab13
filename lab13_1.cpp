6d118c8adf808f4073204e7cf7640f78b54c4ba0
Phakbung <athikan_thoyaem@cmu.ac.th>
2026-01-09 14:19:50 +0700

    lab13_1 completed

--- lab13_1.cpp
+++ lab13_1.cpp
@@ -3,7 +3,8 @@
 #include <cmath>
 using namespace std;
 
-void stat(const double[],int,double[]);
+void stat(const double[],int,double[]) ;
+
 
 int main(){
     double A[] = {1.2,3.5,6.9,7.8,12.5,0.5};
@@ -18,4 +19,35 @@ int main(){
     cout << "Max = " << B[4] << endl;
     cout << "Min = " << B[5];
     return 0;
+}
+void stat(const double A[],int N,double B[]){
+    double sum = 0;
+    double sum_sq_diff = 0;
+    double product = 1;
+    double sum_inverse = 0;
+    double max_val = A[0];
+    double min_val = A[0];
+
+    for(int i = 0; i < N; i++){
+        sum += A[i];
+        product *= A[i];
+        sum_inverse += 1.0 / A[i];
+        
+        if(A[i] > max_val) max_val = A[i];
+        if(A[i] < min_val) min_val = A[i];
+    }
+    B[0] = sum / N;
+
+    for(int i = 0; i < N; i++){
+        sum_sq_diff += pow(A[i] - B[0], 2);
+    }
+    B[1] = sqrt(sum_sq_diff / N);
+
+    B[2] = pow(product, 1.0 / N);
+    
+    B[3] = N / sum_inverse;
+    
+    B[4] = max_val;
+    
+    B[5] = min_val;
 }
